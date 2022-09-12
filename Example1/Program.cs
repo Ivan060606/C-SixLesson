6b54ac79f02686a8903fe92b1c0d07a07925e40d
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-09-12 19:43:13 +0300

    6Lesson

---
+++ Example1/Program.cs
@@ -0,0 +1,16 @@
+﻿void StringToArrayAndPositiveNumbers()
+{
+    Console.Write("Введите числа массива через запятую : ");
+    string arrayNumber = Console.ReadLine();
+    var array = arrayNumber.Split(',').Select(Int32.Parse).ToArray();
+    //Console.WriteLine(array[2]);
+    Console.WriteLine($"Вы ввели массив чисел :[{arrayNumber}]");
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) k++;
+    }
+    Console.WriteLine($"{k} положительных чисел");
+}
+
+StringToArrayAndPositiveNumbers();
---
+++ Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Для уравнений y=k1*x+b1 и y=k2*x+b2 введите числа:");
+
+Console.WriteLine("b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = ((b1-b2)/(k2-k1));
+double y = (k2*b1-k1*b2)/(k2-k1);
+if (k2==k1) Console.WriteLine("Прямые совпадают или параллельны");
+else Console.WriteLine($"Точка пересечения прямых: ({(b1-b2)/(k2-k1)};{y})");
